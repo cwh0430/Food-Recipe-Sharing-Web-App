82eaf745c4a15b3d6504c13cca90de076d028d10
honwenxuan <honwenxuan@gmail.com>
2023-04-04 01:52:42 +0800

    edit favourite function

--- resources/views/recipe/show.blade.php
+++ resources/views/recipe/show.blade.php
@@ -1,3 +1,10 @@
+<?php
+use App\Models\Recipe;
+use Maize\Markable\Models\Favorite;
+
+$user = auth()->user();
+?>
+
 @extends('layouts.app')
 
 @section('content')
@@ -8,34 +15,21 @@
                 <img src="{{ $recipe->image }}" alt="{{ $recipe->name }}" class="img-fluid mb-4">
 
                 <!-- favourite button -->
-                <form action="{{ route('favorite.add', $recipe->id) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <button><a class="btn" data-count="0"><span>&#x2764;</span> Favourite</a></button>
-                </form>
-
-                <!-- try to check if item already in favorite model -->
-                {{-- @auth
-                    @if (auth()->user()->favorites->contains($recipe->id))
-                        <form action="{{ route('favorite.add', $recipe->id) }}" method="POST" enctype="multipart/form-data">
+                @auth
+                    @if (Favorite::has($recipe, $user))
+                        <form action="{{ route('favorite.remove', $recipe->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <button><a class="btn" data-count="0"><span>&#x2764;</span> Favourite</a></button>
-                            <!-- <button class="p-2 bg-red-100 rounded hover:bg-red-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6 text-red-700 hover:text-red-100">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                        </svg>
-
-                    </button> -->
+                            @method('DELETE')
+                            <button type="submit" style="border-color:red; color:red"><a class="btn">Remove from
+                                    Favourites</a></button>
                         </form>
                     @else
-                        <form action="{{ route('favorite.remove', $recipe->id) }}" method="DELETE"
-                            enctype="multipart/form-data">
+                        <form action="{{ route('favorite.add', $recipe->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            <button><a class="btn" data-count="0"><span>&#x2764;</span> Unfavourite</a></button>
+                            <button style="border-color:red; color:red"><a class="btn">Add to Favourites</a></button>
                         </form>
                     @endif
-                @endauth --}}
+                @endauth
 
                 <p>{{ $recipe->description }}</p>
                 <h2>Ingredients:</h2>
@@ -74,81 +68,4 @@
         </div> --}}
         </div>
     </div>
-
-    <style>
-        /* html {
-                                                            background: #f5f5f5;
-                                                            font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-                                                        }
-
-                                                        body {
-                                                            margin: 30px auto 0 auto;
-                                                            width: 450px;
-                                                            font-size: 75%;
-                                                        }
-
-                                                        h3 {
-                                                            margin-top: 30px;
-                                                            font-size: 18px;
-                                                            color: #555;
-                                                        }
-
-                                                        p {
-                                                            padding-left: 10px;
-                                                        } */
-
-
-        /*
-                                                         * Basic button style
-                                                         */
-        .btn {
-            box-shadow: 1px 1px 0 rgba(255, 255, 255, 0.5) inset;
-            border-radius: 3px;
-            border: 1px solid;
-            display: inline-block;
-            height: 18px;
-            line-height: 18px;
-            padding: 0 8px;
-            position: relative;
-
-            font-size: 12px;
-            text-decoration: none;
-            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
-        }
-
-
-
-        .btn {
-            background-color: #dbdbdb;
-            border-color: #bbb;
-            color: #666;
-        }
-
-        .btn:hover,
-        .btn.active {
-            text-shadow: 0 1px 0 #b12f27;
-            background-color: #f64136;
-            border-color: #b12f27;
-        }
-
-        .btn:active {
-            box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.2) inset;
-        }
-
-        .btn span {
-            color: #f64136;
-        }
-
-        .btn:hover,
-        .btn:hover span,
-        .btn.active,
-        .btn.active span {
-            color: #eeeeee;
-        }
-
-        .btn:active span {
-            color: #b12f27;
-            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.3);
-        }
-    </style>
 @endsection
