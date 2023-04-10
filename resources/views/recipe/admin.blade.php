66f51c5a8607ded191eca403b6483299e2c1c659
cwh0430 <jolincwh15@gmail.com>
2023-04-10 22:31:27 +0800

    added delete modal

--- resources/views/recipe/admin.blade.php
+++ resources/views/recipe/admin.blade.php
@@ -32,10 +32,33 @@
           <div class="column text-center">
             <a class="btn btn-success" href="/recipes/{{$recipe->id}}" style="margin-right:5px;">View</a>
             <a class="btn btn-warning" href="/edit/{{$recipe->id}}" style="margin-right:5px;">Edit</a>
-            <a class="btn btn-danger" href="/delete/{{$recipe->id}}" style="margin-right:5px;">Delete</a>
+            <button class="btn btn-danger" data-toggle="modal" data-target="#dltModal-{{$recipe->id}}"
+              style="margin-right:5px;">Delete</button>
           </div>
         </td>
       </tr>
+
+      <!-- Modal -->
+      <div class="modal fade" id="dltModal-{{$recipe->id}}" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Delete Recipe</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Are you sure you want to delete the recipe <strong>{{$recipe->name}}</strong>?
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <a class="btn btn-danger" href="/delete/{{$recipe->id}}" style="margin-right:5px;">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
       @endforeach
     </tbody>
   </table>
